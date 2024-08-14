1a5d678bd8a1234132083799d3cc19ff480f0aa2
paxvin <paxvin44@gmail.com>
2024-08-14 13:35:41 +0200

    Create install-toolchain.sh

---
+++ install-toolchain.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -q https://developer.arm.com/-/media/Files/downloads/gnu-rm/8-2018q4-major/gcc-arm-none-eabi-8-2018-q4-major-linux.tar.bz2
+tar -xjf gcc-arm-none-eabi-8-2018-q4-major-linux.tar.bz2
